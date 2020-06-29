1de50de27d37d52b5658e74b11fd262d608527b6
Heedong Kim <ruthetum@gmail.com>
2020-06-30 01:20:47 +0900

    pug - template variable

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,9 @@
 // global
-export const join = (req, res) => res.render("join");
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
+export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const login = (req, res) => res.render("login", { pageTitle: "Log in" });
+export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
 
 // user
-export const userDetail = (req, res) => res.render("userDetail");
-export const editProfile = (req, res) => res.render("editProfile");
-export const changePassword = (req, res) => res.render("changePassword");
+export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
+export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password" });
