89a2c5b8f66420e5c6ee0ff7583b79f5df400e92
Heedong Kim <ruthetum@gmail.com>
2020-06-29 22:52:39 +0900

    modify error for routing
    1. app.js에서 import morgan X
    2. init.js에서 export default가 아닌 export 값으로 받아서 수정

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import "core-js";
 import express from "express";
+import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
