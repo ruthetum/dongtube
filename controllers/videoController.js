7b2ff22706c038955b6ee1a7f4f7ce969a96c12a
Heedong Kim <ruthetum@gmail.com>
2020-06-30 03:46:14 +0900

    login, upload controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,20 +1,32 @@
 import { videos } from "../db";
+import routes from "../routes";
 
 // global
 export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+    res.render("home", { pageTitle: "Home", videos });
 };
 
 export const search = (req, res) => {
     const {
         query: { term: searchingBy }
-      } = req;
+    } = req;
     res.render("search", { pageTitle: "Search", searchingBy, videos});
 };
 
 
 // video
-export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const getUpload = (req, res) => {
+    res.render("upload", { pageTitle: "Upload" });
+};
+
+
+export const postUpload = (req, res) => {
+    const {
+      body: { file, title, description }
+    } = req;
+    // To Do: Upload and save video
+    res.redirect(routes.videoDetail(324393));
+};
 
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
 
