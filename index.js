d75a3a3799dda1444bbd300381436c1ecd296e20
Heedong Kim <ruthetum@gmail.com>
2020-06-29 22:05:46 +0900

    use babel, nodemon

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-const express = require("express");
+import "core-js";
+import express from "express"
+
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-function handleHome(req, res) {
-    res.send("Hello from home");
-}
+const handleHome = (req, res) => res.send("Hello from home");
   
-function handleProfile(req, res) {
-    res.send("You are on my profile");
-}
-  
+const handleProfile = (req, res) => res.send("You are on my profile");
+
 app.get("/", handleHome);  
 app.get("/profile", handleProfile);
 
