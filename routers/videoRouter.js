b0c6d893e559998b92fde031f51295ca15ec03cb
Heedong Kim <ruthetum@gmail.com>
2020-06-30 23:54:17 +0900

    edit, delete video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import routes from "../routes";
-import { getUpload, postUpload, videoDetail, editVideo, deleteVideo } from "../controllers/videoController";
+import { getUpload, postUpload, videoDetail, getEditVideo, postEditVideo, deleteVideo } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
@@ -8,7 +8,8 @@ const videoRouter = express.Router();
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
