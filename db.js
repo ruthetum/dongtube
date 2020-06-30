a4f3feeb2334b19a40d10d7a953c04bbfb1dcfc4
Heedong Kim <ruthetum@gmail.com>
2020-06-30 21:55:37 +0900

    configure dotenv

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config();
 
-mongoose.connect("mongodb://localhost:27017/dongtube",
+mongoose.connect(
+    process.env.MONGO_URL,
     {
         useNewUrlParser: true,
         useFindAndModify: false
@@ -9,7 +12,7 @@ mongoose.connect("mongodb://localhost:27017/dongtube",
 
 const db = mongoose.connection;
 
-const handleOpen = () => console.log("✅  Connected to DB");
+const handleOpen = () => console.log("✅ Connected to DB");
 const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
 
 db.once("open", handleOpen);
--- init.js
+++ init.js
@@ -1,7 +1,9 @@
 import app from "./app";
 import "./db";
+import dotenv from "dotenv"
+dotenv.config();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => console.log(`✅ Listening on: http://localhost:${PORT}`);
 
