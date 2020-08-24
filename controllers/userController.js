a06041cad917acaaa2af98b3cc246b6490626365
Heedong Kim <ruthetum@gmail.com>
2020-08-24 20:45:02 +0900

    user id에 따른 에러 처리

--- controllers/userController.js
+++ controllers/userController.js
@@ -104,6 +104,16 @@ export const getMe = (req, res) => {
 };
 
 // user
-export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
+export const userDetail = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const user = await User.findById(id);
+    res.render("userDetail", { pageTitle: "User Detail", user });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password" });
