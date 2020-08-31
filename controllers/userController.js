fae4d6ae82caf62d5bc753976bd06d2a6aeb7bf5
Heedong Kim <ruthetum@gmail.com>
2020-08-31 14:26:27 +0900

    Modify User Profile UI

--- controllers/userController.js
+++ controllers/userController.js
@@ -115,5 +115,5 @@ export const userDetail = async (req, res) => {
     res.redirect(routes.home);
   }
 };
-export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
+export const getEditProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password" });
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
 import routes from "../routes";
-import { userDetail, editProfile, changePassword } from "../controllers/userController";
+import { userDetail, getEditProfile, changePassword } from "../controllers/userController";
 import { onlyPrivate } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
